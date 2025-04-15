517c542b692583743d45729836f3090737563d41
Daniel F. Alves <danielalves672@gmail.com>
2025-04-15 20:29:27 -0300

    update

--- js/modules/animacaoScroll.js
+++ js/modules/animacaoScroll.js
@@ -2,7 +2,7 @@ export default function animacaoScroll() {
   const sections = document.querySelectorAll('[data-anime="scroll"]');
   const sectionsRight = document.querySelectorAll('[data-anime="scrollRight"]');
 
-  const metadeTelaUsuario = window.innerHeight * 0.85;
+  const metadeTelaUsuario = window.innerHeight * 0.8;
 
   function scrollAnimado() {
     sections.forEach((section) => {
